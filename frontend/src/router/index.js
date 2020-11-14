5999f4a037985283d72674c1a40c91dbc81c0b3a
Jay <huaiyu11@126.com>
2020-11-14 15:55:10 +0800

    DetailsPaper.vue v1.0

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,11 +7,13 @@ import Index from '../components/Index.vue'
 import Search from '../components/Search.vue'
 import Person from '../components/Person.vue'
 import Paper from '../components/Paper.vue'
+import DetailsPaper from '../components/DetailsPaper'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', redirect: '/home' },
+  // { path: '/', redirect: '/home' },
+  { path: '/', redirect: '/details_paper' },
   { path: '/login', component: Login },
   { path: '/register', component: Register },
   { path: '/home', redirect: '/index', component: Home , children: [
@@ -19,7 +21,8 @@ const routes = [
     { path: '/search', redirect: '/search/person',component: Search , children:[
       { path: 'person', component: Person },
       { path: 'paper', component: Paper }
-    ]}                                    
+    ]},
+    {path: '/details_paper', component: DetailsPaper},                             
   ]},
 ]
 
