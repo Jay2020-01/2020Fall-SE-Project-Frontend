eece754e9d0714ac5085b1870f6a26899ee8d6f4
hjq133 <hjq121200@gmail.com>
2020-12-20 19:31:24 +0800

    update

--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -12,102 +12,46 @@
 <script>
 import ChatWindow from 'vue-advanced-chat'
 import 'vue-advanced-chat/dist/vue-advanced-chat.css'
-
+import axios from "axios";
+import Qs from "qs";
 export default {
   components: {
       ChatWindow
     },
     data () {
       return {
-        rooms:[
-  {
-    roomId: 1,
-    roomName: 'Room 1',
-    avatar: 'assets/imgs/people.png',
-    unreadCount: 4,
-    lastMessage: {
-      content: 'Last message received',
-      sender_id: 1234,
-      username: 'John Doe',
-      timestamp: '10:20',
-      date: 123242424,
-      saved: true,
-      distributed: false,
-      seen: false,
-      new: true
-    },
-    users: [
+    rooms:[
+    {
+      roomId: 1,
+      roomName: 'Json',
+      avatar: 'assets/imgs/people.png',
+    }
+    ],
+    messages: [
       {
-        _id: 1234,
+        _id: 7890,
+        content: 'message 1',
+        sender_id: 1234,
         username: 'John Doe',
-        avatar: 'assets/imgs/doe.png',
-        status: {
-          state: 'online',
-          last_changed: 'today, 14:30'
-        }
       },
       {
-        _id: 4321,
-        username: 'John Snow',
-        avatar: 'assets/imgs/snow.png',
-        status: {
-          state: 'offline',
-          last_changed: '14 July, 20:00'
-        }
+        _id: 7890,
+        content: 'message 1',
+        sender_id: 1234,
+        username: 'John Doe',
       }
     ],
-    typingUsers: [ 4321 ]
-  }
-],
-        messages: [{
-    _id: 7890,
-    content: 'message 1',
-    sender_id: 1234,
-    username: 'John Doe',
-    date: '13 November',
-    timestamp: '10:20',
-    system: false,
-    saved: true,
-    distributed: true,
-    seen: true,
-    disable_actions: false,
-    disable_reactions: false,
-    file: {
-      name: 'My File',
-      size: 67351,
-      type: 'png',
-      audio: true,
-      duration: 14.4,
-      url: 'https://firebasestorage.googleapis.com/...'
-    },
-    reactions: {
-      wink: [
-        1234, // USER_ID
-        4321
-      ],
-      laughing: [
-        1234
-      ]
-    }
-  }],
-        currentUserId: 1234,
-          person_list:[
-            {
-                person:'很长的名字Going Deeper with Convolutions',hIndex:'60',paperNum:'81',reference:'132732',occupation:'教授',company:'北京航空航天大学',fields:['ai','Computer Vision','xxxxxxx','sxdsjniauhvnv','scasds','xxxxxxx','sxdsjniauhvnv','scasds','samxihvrwcccvvfd','zhoujielun','小红','sheqmocubeuxvzksswd','12345','sohe','djivr','dhslaciugy','ahahaha']
-            },
-            {
-                person:'何明凯',hIndex:'0',paperNum:'2',reference:'0',occupation:'教授',company:'北京航空航天大学',fields:['ai','Computer Vision']
-            },
-            {
-                person:'陶哲轩',hIndex:'81',paperNum:'402',reference:'59997',occupation:'教授',company:'北京航空航天大学',fields:['ai','Computer Vision']
-            }
-          ]
+      currentUserId: 1234,
+        
       }
     },
     created: function () {
       this.getTargetUserList();
     },
     methods: {
+      sendMessage({ roomId, content, file, replyMessage}) {
+        console.log("adsfasdfasdfasdfasdfasdfasdfasdf");
+      },
       getTargetUserList() {
         axios.get("http://localhost:8000/ajax/get_person_list/").then((res) => {
           this.person_list = res.data.person_list;
@@ -115,7 +59,7 @@ export default {
       },
       getMessageContent() {
         axios.get("http://localhost:8000/ajax/get_message_content/").then((res) => {
-          // 未完成
+          
         })
       }
   }
